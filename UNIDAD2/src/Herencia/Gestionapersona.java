74fc78399c85d00fa9dcf9463a0e569470579fc3
Manuegijab <mpartor0311@g.educaand.es>
2025-02-12 10:00:26 +0100

    Seguimos con herencias, override y overloading, deberes boletin 5

--- UNIDAD2/src/Herencia/Gestionapersona.java
+++ UNIDAD2/src/Herencia/Gestionapersona.java
@@ -4,6 +4,8 @@ public class Gestionapersona {
 public static void main(String[] args) {
 	Persona p=new Persona ("Pepe", "pepe@gmail.com");
 	Persona p2=new Persona ("Pepe", "pepe@gmail.com");
+	
+	//Sobrecarga 
 	p.concentrarse( 20);
 	p.concentrarse(" Barcelona");
 	p.concentrase();
