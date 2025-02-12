0280c02896278da5e0e582f3c81d228c4fad31e6
Manuegijab <mpartor0311@g.educaand.es>
2025-02-12 09:58:12 +0100

    Seguimos con herencias, damos override y overloading, Deberes: Boletin 5

---
+++ UNIDAD2/src/Herencia/Entrenador.java
@@ -0,0 +1,9 @@
+package Herencia;
+
+public class Entrenador extends Persona {
+
+	public Entrenador(String nombre, String email, int edad, float salario) {
+		super(nombre, email, edad, salario);
+		// TODO Auto-generated constructor stub
+}
+	}
---
+++ UNIDAD2/src/Herencia/Gestionapersona.java
@@ -0,0 +1,11 @@
+package Herencia;
+
+public class Gestionapersona {
+public static void main(String[] args) {
+	Persona p=new Persona ("Pepe", "pepe@gmail.com");
+	Persona p2=new Persona ("Pepe", "pepe@gmail.com");
+	p.concentrarse( 20);
+	p.concentrarse(" Barcelona");
+	p.concentrase();
+}
+}
