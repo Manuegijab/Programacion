f3ddcb13fd2167d7ec41cd1983c95532ed938964
Manuegijab <mpartor0311@g.educaand.es>
2025-02-13 10:02:40 +0100

    CORREGIMOS RPG

--- UNIDAD2/src/RPG/Mago.java
+++ UNIDAD2/src/RPG/Mago.java
@@ -2,4 +2,17 @@ package RPG;
 
 public class Mago extends Personaje {
 
+	public Mago(String nombre, int nivel, int hp) {
+		super(nombre, nivel, hp);
+		// TODO Auto-generated constructor stub
+	}
+	protected boolean esatacado(Personaje atacante) {
+	
+	
+			return false;
+			
+	
+		
+		
+	}
 }
--- UNIDAD2/src/RPG/Villano.java
+++ UNIDAD2/src/RPG/Villano.java
@@ -2,4 +2,12 @@ package RPG;
 
 public class Villano extends Personaje {
 
+	public Villano(String nombre, int nivel, int hp) {
+		super(nombre, nivel, hp);
+		// TODO Auto-generated constructor stub
+	}
+	
+	protected boolean esatacado(Personaje atacante) {
+		return true;
+	}
 }
