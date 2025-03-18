202787978ac5eb3c48848f35cd37d0e4c40cdd29
Manuegitjab <mpartor0311@g.educaand.es>
2025-03-18 23:04:38 +0100

    Ejercicio nominas

--- UNIDAD2/src/Modelo/JefeProyecto.java
+++ UNIDAD2/src/Modelo/JefeProyecto.java
@@ -2,6 +2,29 @@ package Modelo;
 
 public class JefeProyecto extends AEmpleado {
 
+	boolean hablaingles;
+	String certificaciones;
+	Proyecto proyecto;
+	
+	
+	public JefeProyecto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public JefeProyecto(String nombre, String identificador, String apellidos, float salario,
+			boolean tieneReduccionHoraria, Categoria categoria) {
+		super(nombre, identificador, apellidos, salario, tieneReduccionHoraria, categoria);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JefeProyecto(boolean hablaingles, String certificaciones, Proyecto proyecto) {
+		super();
+		this.hablaingles = hablaingles;
+		this.certificaciones = certificaciones;
+		this.proyecto = proyecto;
+	}
+
 	@Override
 	public float calculaimporteNomina() {
 		// TODO Auto-generated method stub
